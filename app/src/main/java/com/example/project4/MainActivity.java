6eecceb2a567cfe9c362fab2fb31fc2c9ef6d7e4
compte-logiciel <compte-logiciel@ens.uqam.ca>
2022-05-28 11:58:38 -0400

    Main menu page

--- app/src/main/java/com/example/project4/MainActivity.java
+++ app/src/main/java/com/example/project4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.project4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent loginIntent = getIntent();
+        String loggedUser = loginIntent.getStringExtra("theUser");
+
+        TextView title = (TextView) findViewById(R.id.MainTitle);
+        title.setText("Welcome " + loggedUser);
     }
 }
